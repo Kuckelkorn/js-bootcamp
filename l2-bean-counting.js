922dde230417d16fe75969ae38cfdc5e246dd42f
Kuckelkorn <46530756+Kuckelkorn@users.noreply.github.com>
2019-02-06 23:56:14 +0100

    Add files via upload

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,25 @@
- 
+function countBs(woord){
+  var count =0;
+  for (var i=0; i<woord.length; i++){
+    if (woord.charAt(i)==="B") {
+      count++;
+    }
+  }
+  return count;
+}
+
+function countChar(woord, letter){
+ var count =0;
+ for (var i=0; i<woord.length; i++){
+   if (woord.charAt(i)===letter){
+
+     count++
+   }
+ }
+ return count;
+}
+
+
+
+console.log(countBs("BluBBer"));
+console.log(countChar("kakkerlak", "k"));

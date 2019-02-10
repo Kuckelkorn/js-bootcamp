b488d2541baef4962ee208b835102a1c62eec74a
Kuckelkorn <46530756+Kuckelkorn@users.noreply.github.com>
2019-02-10 21:42:16 +0100

    Add files via upload

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -1,1 +1,25 @@
- 
+let getallen = [];
+
+function range(start, end, step) {
+  if (start < end) {
+    for (let i = start; i <= end; i += step) {
+      getallen.push(i);
+    }
+  } else if (start > end) {
+    for (let i = start; i >= end; i += step) {
+      getallen.push(i);
+    }
+  }
+  return getallen;
+}
+
+function sum(func) {
+  func;
+  let som = 0;
+  for (let i = 0; i < getallen.length; i++) {
+    som = som + getallen[i];
+  }
+  return som;
+}
+
+console.log(sum(range(1, 10, 1)));
